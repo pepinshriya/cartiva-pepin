acca5af3b575c46827403edeff1939bba71b42f4
Pepin Shriya Arun <pepin.shriya@idp.com>
2026-08-03 15:08:19 +0530

    test: trace CI/CD pipeline with cart-service comment

--- customer-service/src/server.js
+++ customer-service/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 
 const PORT = process.env.PORT || 3006;
+// server running....
 
 app.listen(PORT, () => {
   console.log(`Customer service running on port ${PORT}`);
